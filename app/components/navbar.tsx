5bae6deac61aa52378db9382f66a71fd5a6c8dce
Olivierkepler <olivierkfrancois1@gmail.com>
2025-11-28 01:29:22 -0500

    update

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -151,7 +151,7 @@ export default function Navbar() {
             animate={{ opacity: 0.7 }}
             exit={{ opacity: 0 }}
             transition={{ duration: 0.3 }}
-            className="fixed inset-0 bg-black/60 backdrop-blur-sm z-40"
+            className="fixed inset-0 bg-black/60 backdrop-blur-sm z-50"
             onClick={() => setOpen(false)}
           />
         )}
